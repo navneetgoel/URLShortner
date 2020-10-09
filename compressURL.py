8ddf9a96668b1d04a15d698aedced89cf42cdffd
navneetgoel <goel.navneet4@gmail.com>
2020-10-09 11:51:12 -0700

    Added Redis Config

--- compressURL.py
+++ compressURL.py
@@ -7,7 +7,6 @@ import sys
 class compressURL:
     def __init__(self):
         self.redis = redis.StrictRedis(host=config.REDIS_HOST,port=config.REDIS_PORT,db=config.REDIS_DB)
-        pass
 
     def shrinkURL(self, url):
         print(url)
