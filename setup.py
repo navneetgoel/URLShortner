dce57510909f5027dd60e7d0a2b58b16d26e462f
navneetgoel <goel.navneet4@gmail.com>
2020-10-12 21:57:49 -0700

    Added setup file

--- setup.py
+++ setup.py
@@ -5,3 +5,6 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as reqs:
     install_requires = [line for line in reqs.read().split('\n') if (line and not line.startswith('--'))]
     print(install_requires)
+
+def read(*parts):
+    return codecs.open(path.join(path.dirname(__file__), *parts), enoding="utf-8").read()
