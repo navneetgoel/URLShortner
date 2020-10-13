cb2a3c1af352e4afe36f7723c080f00f971dc8f1
navneetgoel <goel.navneet4@gmail.com>
2020-10-12 22:08:12 -0700

    added flask app

--- shrinkURL/__init__.py
+++ shrinkURL/__init__.py
@@ -1,2 +1,13 @@
 import logging
 from flask import Flask, json, request, redirect, render_template,make_response
+from compressURL import compressURL
+from functools import wraps
+from bernhard import Client
+from riemann_wrapper import riemann_wrapper
+import urllib.parse
+
+app = Flask(__name__)
+shrt = compressURL()
+logger = logging.getLogger()
+
+logger.info("Starting the app")
